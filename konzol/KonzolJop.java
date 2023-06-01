248174c9a8f5242d550126bce4eb0e7d558e9930
PatrisaRose <94226854+PatrisaRose@users.noreply.github.com>
2023-06-01 19:59:26 +0200

    Add files via upload

---
+++ konzol/KonzolJop.java
@@ -0,0 +1,11 @@
+package konzol;
+
+import javax.swing.JOptionPane;
+
+public class KonzolJop {
+
+    public static void main(String[] args) {
+       JOptionPane.showMessageDialog(null, "Lófasz");
+    }
+    
+}
